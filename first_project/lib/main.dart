5b82457f63de55f6b19967974f42d551630cbd7f
CynaraCosta <cvocac@cin.ufpe.br>
2023-08-09 18:08:34 -0300

    first commit

---
+++ first_project/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text(
+    "Fluterando", 
+    textDirection: TextDirection.ltr,
+    style: TextStyle(
+      color: Colors.green,
+      fontSize: 30.0
+      )
+    ),
+  ));
+}
