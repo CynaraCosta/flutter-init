80bbb798e724d1558cdca4d46b3dce17dc12cfeb
CynaraCosta <cvocac@cin.ufpe.br>
2023-08-19 18:51:37 -0300

    passing route data

--- world_time/world_time/lib/pages/loading.dart
+++ world_time/world_time/lib/pages/loading.dart
@@ -10,13 +10,13 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
-
   Future<void> setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'europe/berlin');
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time
     });
   }
 
@@ -33,7 +33,7 @@ class _LoadingState extends State<Loading> {
         child: Padding(
           padding: EdgeInsets.all(50),
           child: Text(
-            time
+            'loading'
           ),
         )
       ),
