846e25edd15092b583911bbdec137cc6eae039c9
CynaraCosta <cvocac@cin.ufpe.br>
2023-08-20 17:36:05 -0300

    flutter spin_kit

--- world_time/world_time/lib/pages/home.dart
+++ world_time/world_time/lib/pages/home.dart
@@ -18,15 +18,46 @@ class _HomeState extends State<Home> {
     print(data);
     
     return Scaffold(
-      body: SafeArea(child: Column(
-        children: <Widget>[
-            TextButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-            }, 
-              icon: const Icon(Icons.edit_location), 
-              label: const Text('Edit location'))
-        ],
+      body: SafeArea(child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+        child: Column(
+          children: <Widget>[
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+              }, 
+                icon: Icon(
+                  Icons.edit_location,
+                  color: Colors.grey[800],
+                  ), 
+                label: Text(
+                  'Edit location',
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                  ),
+                  )),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28,
+                        letterSpacing: 2
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66,
+                  ),
+                )
+          ],
+        ),
       )),
     );
   }
